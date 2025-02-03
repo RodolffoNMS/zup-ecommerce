0958407826654b0e23d4783737113f4c913e7ac5
RodolffoNMS <rodolffo.souza@zup.com.br>
2025-02-03 08:36:18 -0300

    feat: adiciona OutofStockException

--- src/main/java/com/zup/ecommerce/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/zup/ecommerce/exceptions/GlobalExceptionHandler.java
@@ -46,4 +46,11 @@ public class GlobalExceptionHandler {
         ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(OutOfStockException.class)
+    public ResponseEntity<Map<String, String>> handleOutOfStockException(OutOfStockException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
